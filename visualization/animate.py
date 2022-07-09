925a309fb3c4d3cf4373d4656fd0e6abc0e36c26
Justin Yu <yujustin8@gmail.com>
2022-07-09 16:07:08 -0700

    sad

--- visualization/animate.py
+++ visualization/animate.py
@@ -13,8 +13,8 @@ class Visualizer(object):
     def __init__(self):
         global x, y, z, yaw, pitch, roll, cl, r, count
         count = r = x = y = z = yaw = pitch = roll = cl = 0
-        self.robot = Quadruped(ax=self, origin=(25, 0, 70))
-        self.r2 = Quadruped(ax=self, origin=(25, 0, 70))
+        self.robot = Quadruped(ax=self, origin=(30, 0, 60))
+        self.r2 = Quadruped(ax=self, origin=(30, 0, 60))
         self.px = x
         self.py = y
         self.pz = z
--- visualization/main.py
+++ visualization/main.py
@@ -20,8 +20,8 @@ class QThread(threading.Thread):
         super(QThread, self).__init__(name=name)
         self.daemon = True
         time.sleep(2)
-        self.r1 = Quadruped(ax=self, origin=(25, 0, 70))
-        self.r2 = Quadruped(ax=self, origin=(25, 0, 70))
+        self.r1 = Quadruped(ax=self, origin=(30, 0, 60))
+        self.r2 = Quadruped(ax=self, origin=(30, 0, 60))
         self.px = x
         self.py = y
         self.pz = z
